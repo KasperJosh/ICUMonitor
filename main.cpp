1e5a9bfb99b5103b983732cfb73f23bd45d2ec8a
KasperJosh <visario.joshua@gmail.com>
2026-09-23 12:19:59 -0400

    Finished V/S Classficiation

--- main.cpp
+++ main.cpp
@@ -15,8 +15,63 @@ void checkHeartRate(int heartRate)
     }
     else
     {
-        std::cout << "Heart Rate: NORMOCARDIA";
+        std::cout << "Heart Rate: NORMOCARDIA\n";
     }
+}
+
+void checkSpO2 (int spo2)
+{
+    if (spo2 < 88)
+    {
+        std::cout << "SpO2: DESATURAION\n";
+    }
+    else if (spo2 >= 88 && spo2 <= 100) {
+        std::cout << "SpO2: NORMAL SATURATION\n";
+    }
+}
+
+void checkBP (int systolicBP, int diastolicBP)
+{
+    if (systolicBP < 90 || diastolicBP < 60){
+        std::cout << "Blood Pressure: Low blood pressure\n";
+    } 
+    else if (systolicBP > 180 || diastolicBP > 120){
+        std::cout << "Blood Pressure: Severe hypertension\n";
+    }
+    else if (systolicBP >= 140 || diastolicBP >= 90){
+        std::cout << "Blood Pressure: Stage 2 hypertension\n";
+    }
+    else if (systolicBP >= 130 || diastolicBP >= 80){
+        std::cout << "Blood Pressure: Stage 1 hypertension\n";
+    }    
+    else if (systolicBP >= 120){
+        std::cout << "Blood Pressure: Elevated\n";
+    } 
+    else{
+        std::cout << "Blood Pressure: Normal\n";
+    }
+}
+
+void checkRespRate(int respRate){
+
+    if (respRate < 12 )
+    {
+        std::cout << "Respiratory Rate: BRADYPNEA\n";
+    }
+    else if (respRate < 20 )
+    {
+        std::cout << "Respiratory Rate: TACHYPNEA\n";
+    }
+    else
+    {
+        std::cout << "Respiratory Rate: EUPNEIC\n";
+    }
+}
+
+
+double calculateMAP(int systolicBP, int diastolicBP)
+{
+    return (systolicBP + 2.0*diastolicBP) / 3.0;
 }
 
 
@@ -27,12 +82,39 @@ int main()
     // Functions → Display Results → End Program
 
     // Asking for the heart rate
+    int systolicBP;
+    int diastolicBP;
     int heartRate;
+    int spo2;
+    int respRate;
+
+    std::cout <<"Enter Systolic Blood Pressure: ";
+    std::cin >> systolicBP;
+
+    std::cout <<"Enter Diastolic Blood Pressure: ";
+    std::cin >> diastolicBP;
 
     std::cout <<"Enter Heart Rate: ";
     std::cin >> heartRate;
 
-    checkHeartRate(heartRate);
+    std::cout <<"Enter SpO2: ";
+    std::cin >> spo2;
+
+    std::cout <<"Enter Respiratory Rate : ";
+    std::cin >> respRate;
+
+    std::cout << "\n";
+
+    std::cout <<"--------Vital Signs Classification -------\n";
+    checkBP(systolicBP, diastolicBP);
+    double map = calculateMAP(systolicBP, diastolicBP);
+    std::cout << "MAP: " << map << '\n';
     
+    checkHeartRate(heartRate);
+    checkSpO2(spo2);
+    checkRespRate(respRate);
+    
+    
+
     return 0;
 }
